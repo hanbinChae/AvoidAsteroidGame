2c6c71d7efef1d9907d83ab6e53ac463a8222746
hanbinChae <hanbin2043@gmail.com>
2022-05-26 20:04:29 +0900

    소행성1개 충돌 시 충돌 메세지 출력 + 진행시간

--- Asteroid.py
+++ Asteroid.py
@@ -1,56 +1,109 @@
-import turtle
+from lib2to3.pgen2.token import STAR
+import pygame
+import sys # 파이썬 인터프리터 제어
 import time
 
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 300
+imgShuttle = pygame.image.load("DogeRocket.png")
+img_width = imgShuttle.get_width()
+img_height = imgShuttle.get_height()
 
-def outline():
-    tline = turtle.Turtle()
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 480
+START_TIME = time.time()
+
+pygame.init()
+pygame.display.set_caption("SpaceShuttle") # 창 제목 설정
+
+#지정한 크기의 창 생성
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+myFont = pygame.font.SysFont("arial", 30, True, False)
+clock = pygame.time.Clock()
+
+#속도 지정
+vel = [4,4]
+
+#소행성 5개 생성
+loc_rock = [100,100]
+size_rock = 10
+
+#도지 우주선 생성
+loc_ship = [SCREEN_WIDTH/2,SCREEN_HEIGHT/2]
+size_ship = 20
+
+#폰트 설정
+myFont = pygame.font.SysFont("arial", 30, True, False)
+
+#소행성에 닿았을 때 게임 종료
+def collision_check(loc_rock,size_rock,loc_ship,size_ship):
+    dist_x = loc_rock[0] - loc_ship[0]
+    dist_y = loc_rock[1] - loc_ship[1]
+    dist = (dist_x**2 + dist_y**2)**(0.5)
     
-    tline.up()
-    tline.goto(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
-    tline.down()
-    
-    tline.goto(-SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
-    tline.goto(-SCREEN_WIDTH/2,-SCREEN_HEIGHT/2)
-    print(-SCREEN_WIDTH/2,-SCREEN_HEIGHT/2)
-    tline.goto(SCREEN_WIDTH/2,-SCREEN_HEIGHT/2)
-    tline.goto(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
-
-outline()
-
-loc=[[0,0],[0,0]]
-vel=[[10,7],[-5,9]]
-asteroid_list = []
-startTime = time.time()
-
-for i in range(len(loc)):
-    t = turtle.Turtle()
-    t.shape("circle")
-    t.goto(loc[i][0],loc[i][1])
-    asteroid_list.append(t)
-
-for i in range(200):
-   for i in range(len(loc)):
-        loc[i][0]+=vel[i][0]
-        loc[i][1]+=vel[i][1]
+    if dist < (size_rock + size_ship):
+        collision_text = myFont.render("Collision", 1, (255,0,0))
+        screen.blit(collision_text, [20,20])
         
-        asteroid_list[i].goto(loc[i][0],loc[i][1])
         
-        if loc[i][0]>=SCREEN_WIDTH/2:
-            vel[i][0] = -vel[i][0]
-        
-        if loc[i][0]<=-SCREEN_WIDTH/2:
-            vel[i][0] = -vel[i][0]
-        
-        if loc[i][1]>=SCREEN_HEIGHT/2:
-            vel[i][1] = -vel[i][1]
-        
-        if loc[i][1]<=-SCREEN_HEIGHT/2:
-            vel[i][1]= -vel[i][1]
+while True:
+    clock.tick(30)
+    screen.fill((0,0,0))
             
-timeDiff = time.time()-startTime
-turtle.write(timeDiff)
+    keys = pygame.key.get_pressed()      
+    
+    if keys[pygame.K_LEFT]:
+        loc_ship[0] -=3
+        
+    if keys[pygame.K_RIGHT]:
+        loc_ship[0] +=3
+        
+    if keys[pygame.K_UP]:
+        loc_ship[1] -=3
+        
+    if keys[pygame.K_DOWN]:
+        loc_ship[1] +=3
+    
+    if keys[pygame.K_q]:
+        pygame.quit()
+        sys.exit()
+        
+    for event in pygame.event.get(): #발생한 입력 event 목록의 event 마다 검사
+        if event.type == pygame.QUIT: #event의 type이 QUIT에 해당할 경우
+            pygame.quit() # pygame 종료
+            sys.exit()
+    
+    #소행성 움직이기 (소행성 위치 + 속도를 반복적으로 추가하여)
+    loc_rock[0]+=vel[0]
+    loc_rock[1]+=vel[1]
 
-turtle.exitonclick()
-turtle.bye()
+    #소행성이 벽면에 닿을 경우 진행 방향으로 반대로 이동
+    if loc_rock[0] >= SCREEN_WIDTH:
+        vel[0] = -vel[0]
+        
+    if loc_rock[0] <= 0:
+        vel[0] = -vel[0]       
+    
+    if loc_rock[1] >= SCREEN_HEIGHT:
+        vel[1] = -vel[1]
+    
+    if loc_rock[1] <= 0:
+        vel[1] = -vel[1]
+
+    # 게임 진행 시간 표시
+    time_diff = str(time.time()-START_TIME)
+    currentTime_text = myFont.render(time_diff[:4],1,(255,255,255))
+    screen.blit(currentTime_text, [SCREEN_WIDTH-70,20])
+
+    #소행성
+    pygame.draw.circle(screen, (0,255,0), loc_rock, size_rock,2)
+
+    #소행성 충돌 시
+    collision_check(loc_rock, size_rock, loc_ship, size_ship)
+
+    #도지 우주선
+    pygame.draw.circle(screen,(255,255,255),loc_ship, size_ship,1)
+    x = loc_ship[0] - img_width/2
+    y = loc_ship[1] - img_height/2
+    screen.blit(imgShuttle,(x,y))
+
+    pygame.display.update() # 화면 업데이트
+print(f"게임 종료!! 도지로켓이 생존한 시간 : {time_diff[:4]}초")
